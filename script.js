8f09b396508f05311427d87ab94200f22bdcc47a
SyedaAkther <syedaakther120@gmail.com>
2024-07-31 12:42:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
 
+//console log text, expression 
+// console.log("hello world")
+// console.log(3+6)
+
+// Create a new variable to hold your name as a string
+
+// myName = "Syeda" //reassign value of myName
+// console.log(myName)
+
+// const myName = "Syeda"
+// myName = "Syeda"
+
+// var myName = "Syeda"
+let myName = "Syeda"
+let myMessage = "Good evening, "
+myMessage = myMessage + myName
+
+// console.log(myMessage)
+
+
+//Concatenate your name variable into a message and log it to the console.
+//Example: “Hello, Syeda!” 
+
+//console log your name variable
+
+//Re-assign the value of your name variable below its declaration and console log it again
+
+//practice math operators
+// console.log(10-7) // subtraction
+// console.log(2*5) //multiplication 
+// console.log(18/6) //division
+// console.log(8**2) //exponent
+
+let myNumber = 9**2 // 81
+
+myNumber = myNumber - 20 //81 -20 
+// console.log(myNumber)
+
+
+//boolean - true or false
+let isSunny = false 
+// console.log(!isSunny)
+
+//create null and undefined
+let isHungry 
+// console.log(isHungry)
+
+//mix data types
+// console.log(2 + "10")
+
+let quantity = "12"; // 12 is a string bc its in quotes
+console.log(typeof quantity);
+
+quantity = Number(quantity) //converting quantity to a number data type
+console.log(typeof quantity);
+
+//making a change
+
+//string methods
+
+//link up html file and create prompt and save value
+
+//access the DOM
